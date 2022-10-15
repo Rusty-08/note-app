0c0a8a0d96bf507e98a67e70e4d96bb2c29fbde0
Rusty Gunao <gunaorusty@gmail.com>
2022-10-15 17:40:57 +0800

    Delete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const add = require('./add')
 const write = require('./write')
 const read = require('./read')
+const del = require('./del')
 
 const cmd = process.argv
 
@@ -23,4 +24,13 @@ if(cmd[2] == 'read') {
     const present = require('./present')
     
     present(read())
+}
+
+if(cmd[2] === 'delete') {
+    let id = cmd[3]
+    let oldNote = read()
+    let del = require('./del')
+    del(id, oldNote)
+
+    console.log(read())
 }
